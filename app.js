6de11da6d4cb543900f290d28350b774ef367250
Oliver De Leon <oliverdeleong@gmail.com>
2022-07-28 13:24:38 -0400

    connect to local mongodb server and save posts

--- app.js
+++ app.js
@@ -1,24 +1,59 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
 
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"));
 
-var entries = [];
+mongoose.connect("mongodb://localhost:27017/blogDB");
 
-var newEntry = new Entry("Day 1", "Nisl nisi scelerisque eu ultrices. Elementum curabitur vitae nunc sed velit. Scelerisque fermentum dui faucibus in. Sit amet venenatis urna cursus. In iaculis nunc sed augue lacus. Tincidunt id aliquet risus feugiat in. Volutpat sed cras ornare arcu dui vivamus arcu felis. Mattis pellentesque id nibh tortor id. Ornare arcu odio ut sem nulla pharetra diam. Lacus luctus accumsan tortor posuere ac ut consequat semper.");
-entries.push(newEntry);
-newEntry = new Entry("This is a post", "Congue eu consequat ac felis donec et odio pellentesque. Viverra mauris in aliquam sem fringilla ut morbi. Turpis egestas sed tempus urna et pharetra pharetra massa. Velit scelerisque in dictum non consectetur. Condimentum id venenatis a condimentum vitae sapien pellentesque habitant. In iaculis nunc sed augue lacus viverra. Leo duis ut diam quam nulla porttitor massa id. Eget felis eget nunc lobortis mattis. Lobortis feugiat vivamus at augue eget. At imperdiet dui accumsan sit amet nulla. Et sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque. Suspendisse in est ante in nibh mauris. Proin nibh nisl condimentum id venenatis a condimentum vitae.");
-entries.push(newEntry);
+const postSchema = mongoose.Schema({
+    heading: String,
+    content: String
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+const defaultPost1 = new Post({
+    heading: "Day 1",
+    content: "Nisl nisi scelerisque eu ultrices. Elementum curabitur vitae nunc sed velit. Scelerisque fermentum dui faucibus in. Sit amet venenatis urna cursus. In iaculis nunc sed augue lacus. Tincidunt id aliquet risus feugiat in. Volutpat sed cras ornare arcu dui vivamus arcu felis. Mattis pellentesque id nibh tortor id. Ornare arcu odio ut sem nulla pharetra diam. Lacus luctus accumsan tortor posuere ac ut consequat semper."
+});
+
+const defaultPost2 = new Post({
+    heading: "This is a post",
+    content: "Congue eu consequat ac felis donec et odio pellentesque. Viverra mauris in aliquam sem fringilla ut morbi. Turpis egestas sed tempus urna et pharetra pharetra massa. Velit scelerisque in dictum non consectetur. Condimentum id venenatis a condimentum vitae sapien pellentesque habitant. In iaculis nunc sed augue lacus viverra. Leo duis ut diam quam nulla porttitor massa id. Eget felis eget nunc lobortis mattis. Lobortis feugiat vivamus at augue eget. At imperdiet dui accumsan sit amet nulla. Et sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque. Suspendisse in est ante in nibh mauris. Proin nibh nisl condimentum id venenatis a condimentum vitae."
+});
+
+var defaultPosts = [defaultPost1, defaultPost2];
 
 app.get("/", function(req, res){
-    res.render("index.ejs", {entries: entries});
+    
+    Post.find({}, function(err, results){
+        if (results.length === 0){
+            Post.insertMany(defaultPosts, function(error){
+                if (error){
+                    console.log(error);
+                } else{
+                    console.log("Successfully inserted default posts.");
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("index.ejs", {entries: results});
+        }
+    });
+
 });
 
 app.post("/", function(req, res){
-    newEntry = new Entry(req.body.title, req.body.content);
-    entries.push(newEntry);
+    const post = new Post({
+        heading: req.body.title,
+        content: req.body.content
+    });
+
+    post.save();
+
     res.redirect("/");
 });
 
@@ -27,11 +62,14 @@ app.get("/new", function(req, res){
 });
 
 app.get("/posts/:postname", function(req, res){
-    for (let i = 0; i < entries.length; i++){
-        if (entries[i].route == req.params.postname){
-            return res.render("individualpost.ejs", {heading: entries[i].heading, content: entries[i].content});
-        }
-    }
+
+    Post.find({}, function(err, results){
+        results.forEach(function(result){
+            if (result.heading.replaceAll(" ", "-").toLowerCase() === req.params.postname){
+                return res.render("individualpost.ejs", {heading: result.heading, content: result.content});
+            }
+        });
+    });
 
 });
 
@@ -46,10 +84,3 @@ app.get("/contact", function(req, res){
 app.listen(3000, function(){
     console.log("Listening on port 3000");
 });
-
-function Entry(heading, content){
-    this.heading = heading;
-    this.content = content;
-    this.route = heading.replaceAll(" ", "-").toLowerCase();
-
-}
